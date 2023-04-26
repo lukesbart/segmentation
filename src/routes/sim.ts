6e28f486895e49f9951069b3c5a021a9e29a7e11
Luke Bartholomew <lukesbart@icloud.com>
2023-04-26 15:30:32 -0400

    Change sim and skeleton to ts files

--- src/routes/sim.ts
+++ src/routes/sim.ts
@@ -1,16 +1,21 @@
-import pkg from './skeleton.mjs'
-const { PhysicalAddressSpace, VirtualAddressSpace, Segment, segmentType, implicitActions, growDirection} = pkg; 
+import pkg from './skeleton'
+const { PhysicalAddressSpace, VirtualAddressSpace, Segment} = pkg; 
+
+import growDirection from './constants/growDirection';
+import segmentType from './constants/segmentType';
+import { boundsCalc } from './constants/baseBoundsCalc';
 
 class Simulator {
+  pas: any;
+
   static segmentType = segmentType;
-  static implicitActions = implicitActions;
   static growDirection = growDirection;
 
   constructor(paLength = 12, vaLength = 10) {
     this.pas = new PhysicalAddressSpace(paLength, vaLength)
   }
 
-  editVaLength(newVaLength) {
+  editVaLength(newVaLength: number) {
     if (newVaLength > this.pas.paLength) {
       throw new Error("VALength must be less than PALength")
     }
@@ -29,7 +34,7 @@ class Simulator {
     this.pas.editVaLength(newVaLength);
   }
 
-  editPaLength(newPaLength) {
+  editPaLength(newPaLength: number) {
     if (newPaLength < this.pas.vaLength) {
       throw new Error("PA Length Cannot Be Less than VA Length")
     }
@@ -44,7 +49,7 @@ class Simulator {
   }
 
   // Segments should be created when a new address space is created with a default size, the user can only edit segments or delete the virtual address space containing them
-  deleteVirtualAddressSpace(vas) {
+  deleteVirtualAddressSpace(vas: InstanceType<typeof VirtualAddressSpace>) {
     // Remove all the segments in the virtual address space from the physical address space
     for (let i = 0; i < vas.segmentList.length; i++) {
       this.pas.segmentList.splice(this.pas.segmentList.indexOf(vas.segmentList[i]), 1);
@@ -55,7 +60,7 @@ class Simulator {
 
   // Need a way to determine which vas a segment is in
   // Since the simulator will know what virtual address space is in, might not need to find it, could be useful to confirm that segment exists in PAS
-  editSegment(segment, newBase, newSize) {
+  editSegment(segment: InstanceType<typeof Segment>, newBase: number, newSize: number) {
     if (newBase < 0) {
       throw new Error("Base cannot be negative")
     }
@@ -76,7 +81,7 @@ class Simulator {
     }
 
     // Need to make sure that base is in bounds of PA
-    if (this.pas.validSegmentCreationOrChange(segment, newBase, newSize*segment.growDirection) && vas.validSegmentCreationOrChange(segment, newSize) && ((newBase + newSize*segment.growDirection)) < this.pas.paSize) {
+    if (this.pas.validSegmentCreationOrChange(segment, newBase, newSize, segment.growDirection) && vas.validSegmentCreationOrChange(segment, newSize) && ((newBase + newSize*segment.growDirection)) < this.pas.paSize) {
       // Don't need a function in pas to change segment because current information is valid, changes automatically
       
       let segmentIndex = vas.segmentList.indexOf(segment);
@@ -86,15 +91,15 @@ class Simulator {
       // Need to handle growth direction for segment
       segment.size = newSize;
       segment.base = newBase;
-      segment.bounds = segment.size*segment.growDirection + segment.base;
+      segment.bounds = boundsCalc(newBase, newSize, segment.growDirection); 
 
       vas.editSegment(segment, segmentIndex);
     } else {
-      throw new Error("Invalid segment resize")
+      throw new Error("Invalid segment base or size")
     }
   }
 
-  editSegmentGrowDirection(segment, growDirection) {
+  editSegmentGrowDirection(segment: InstanceType<typeof Segment>, newGrowDirection: number) {
     // Have to find vas because vaBase and vaBounds are based partially on growDirection
     let vas;
     for (let i = 0; i < this.pas.addressSpaceList.length; i++) {
@@ -106,12 +111,21 @@ class Simulator {
     
     let segmentIndex = vas.segmentList.indexOf(segment);
 
-    segment.growDirection = growDirection;
+    segment.growDirection = newGrowDirection;
 
-    vas.editSegmentGrowDirection(segment, segmentIndex, growDirection);
+    vas.editSegmentGrowDirection(segment, segmentIndex, newGrowDirection);
 
-    let newBase = segment.bounds;
-    let newBounds = segment.base;
+    // Need way to switch these while being able to reverse them an infinte amount of times
+    let newBase, newBounds : number;
+
+    // With negative grow direction, the actual base is one more than the actual number of addresses because the 0th address is not valid
+    if (newGrowDirection === growDirection.Negative) {
+      newBase = segment.bounds + 1;
+      newBounds = boundsCalc(newBase, segment.size, newGrowDirection)
+    } else {
+      newBase = segment.bounds;
+      newBounds = boundsCalc(newBase, segment.size, newGrowDirection)
+    }
 
     segment.base = newBase;
     segment.bounds = newBounds;
@@ -123,8 +137,8 @@ class Simulator {
     this.pas.addNewVAS(newVirtualAddressSpace);
   }
 
-  createNewSegment(type, base, size, growDirection, vas) {
-    if (this.pas.validSegmentCreationOrChange(-1, base, size*growDirection) && vas.validSegmentCreationOrChange(-1, size)) {
+  createNewSegment(type, base: number, size: number, growDirection: number, vas) {
+    if ((this.pas.validSegmentCreationOrChange(-1, base, size*growDirection) && vas.validSegmentCreationOrChange(-1, size)) || (base === -1 && size === 0)) {
       const newSegment = new Segment(type, base, size, growDirection);
 
       vas.addNewSegment(newSegment);
@@ -132,12 +146,27 @@ class Simulator {
     }
   }
 
-  translateVirtualToPhysicalAddress(vasIndex, virtualAddress) {
+  translateVirtualToPhysicalAddress(vasIndex: number, virtualAddress: number) {
+    if (virtualAddress < 0) {
+      return "N/A"
+    }
+
     return this.pas.addressSpaceList[vasIndex].translateVirtualToPhysicalAddress(virtualAddress);
   }
 
-  addressInSegment(vasIndex, virtualAddress) {
+  addressInSegment(vasIndex: number, virtualAddress: number) {
     return this.pas.addressSpaceList[vasIndex].addressInSegment(virtualAddress);
+  }
+
+  explicitAddress(segment: InstanceType<typeof Segment>, address: number) {
+    if (address < 0) {
+      return [null, null];
+    }
+
+    let code = segment.type.number.toString(2).padStart(2, '0');
+    let offset = Math.abs(address - segment.vaBase).toString(2).padStart(this.pas.vaLength -  2, '0')
+
+    return [code, offset];
   }
 
   toJSON() {
@@ -146,7 +175,7 @@ class Simulator {
 
   // Should the client take this and set their PAS equal to newSim.pas or the simulator does it automatically?
   // These functions should be moved into their own class?
-  createBuild(jsonInput) {
+  createBuild(jsonInput: string) {
     let parseObj;
 
     try {
@@ -164,17 +193,17 @@ class Simulator {
 
       parseObj["addressSpaceList"][i]["segmentList"].forEach(segment => {
         // Bug from improper growDirection and segmentType?
-        let segmentType = Simulator.segmentType[segment["type"].name.toLowerCase()];
+        let newSegmentType = segmentType[segment["type"].name.toLowerCase()];
        
 
-        let segmentGrowDirection;
+        let segmentGrowDirection: number;
         if (segment["growDirection"] === 1) {
           segmentGrowDirection = Simulator.growDirection.Positive;
         } else if (segment["growDirection"] === -1) {
           segmentGrowDirection = Simulator.growDirection.Negative;
         }
 
-        newSim.createNewSegment(segmentType, segment["base"], segment["size"], segmentGrowDirection, newSim.pas.addressSpaceList[i])
+        newSim.createNewSegment(newSegmentType, segment["base"], segment["size"], segmentGrowDirection!, newSim.pas.addressSpaceList[i])
         
       })
     }
@@ -199,7 +228,7 @@ class Simulator {
       this.createNewSegment(segmentType.heap, base, size, growDirection.Positive, this.pas.addressSpaceList[i])
       base += size;
 
-      this.createNewSegment(segmentType.stack, base+size, size, growDirection.Negative, this.pas.addressSpaceList[i])
+      this.createNewSegment(segmentType.stack, base+size-1, size, growDirection.Negative, this.pas.addressSpaceList[i])
       base += size;
 
       this.createNewSegment(segmentType.extra, base, size, growDirection.Positive, this.pas.addressSpaceList[i])
@@ -235,37 +264,20 @@ class Simulator {
     this.createNewSegment(segmentType.stack, base+size, size, growDirection.Negative, as)
     base += size;
 
-    this.createNewSegment(segmentType.extra, base+1, size-1, growDirection.Positive, as)
+    this.createNewSegment(segmentType.extra, base, size, growDirection.Positive, as)
   }
 
   createNewBlankAddressSpace() {
-    let size = 0;
-    let base = 0;
-
-    if (this.pas.segmentList.length > 0) {
-      let lastSegment = this.pas.segmentList[this.pas.segmentList.length-1]
-
-      if (lastSegment.growDirection === growDirection.Positive) {
-        base = lastSegment.bounds + 1;
-      } else {
-        base = lastSegment.base + 1;
-      }
-    }
+    
 
     this.createAddressSpace();
 
     let as = this.pas.addressSpaceList[this.pas.addressSpaceList.length - 1]
 
-    this.createNewSegment(segmentType.code, base, size, growDirection.Positive, as);
-    base++;
-
-    this.createNewSegment(segmentType.heap, base, size, growDirection.Positive, as);
-    base++;
-
-    this.createNewSegment(segmentType.stack, base, size, growDirection.Negative, as);
-    base++;
-
-    this.createNewSegment(segmentType.extra, base, size, growDirection.Positive, as);
+    this.createNewSegment(segmentType.code, -1, 0, growDirection.Positive, as);
+    this.createNewSegment(segmentType.heap, -1, 0, growDirection.Positive, as);
+    this.createNewSegment(segmentType.stack, -1, 0, growDirection.Negative, as);
+    this.createNewSegment(segmentType.extra, -1, 0, growDirection.Positive, as);
   }
 }
 
---
+++ src/routes/skeleton.ts
@@ -0,0 +1,281 @@
+import segmentType from "./constants/segmentType";
+import growDirection from "./constants/growDirection";
+import { inRange } from "./constants/inRange";
+import { validAccess } from "./constants/validAccess";
+import { boundsCalc } from "./constants/baseBoundsCalc";
+
+class PhysicalAddressSpace {
+  paLength: number;
+  paSize: number;
+  vaLength: number;
+  vaSize: number;
+  segmentList: Array<Segment>
+  maxAddressSpaces: number;
+  vasId: number;
+  addressSpaceList: Array<VirtualAddressSpace>
+
+  constructor(paLength: number, vaLength: number) {
+    this.paLength = paLength;
+    this.paSize = (2 ** paLength);
+
+    this.vaLength = vaLength;
+    this.vaSize = (2 ** vaLength);
+
+    this.segmentList = [];
+
+    // Change this to a just be reliant on the getPercentFull function
+    this.maxAddressSpaces = (this.paLength - this.vaLength) === 0 ? 1 : (this.paLength - this.vaLength) * 2; 
+
+    this.addressSpaceList = [];
+
+    // Keep track of vasId to ensure unique vas names
+    this.vasId = 0;
+  }
+
+  validSegmentCreationOrChange(segment: InstanceType<typeof Segment>, newBase: number, newSize: number, segmentGrowDirection: number): boolean {
+    // Segment can't be bigger than its share of the virtual address space
+    if (newSize > (this.vaSize / 4)) {
+      throw new Error("Segment cannot be bigger than 1/4 of the Virtual Address Space Size")
+    }
+
+    // Using for i loop to avoid having to keep a separate valid tracking variable
+    for (let i = 0; i < this.segmentList.length; i++) {
+      if (this.segmentList[i] === segment) {
+        continue;
+      }
+
+      // Positive and negative grow direction segments share same base if they are adjacent
+      // Segments can't be created in overlapping space
+      // if (segment.growDirection === -1 && (inRange(newBase, this.segmentList[i].base, this.segmentList[i].bounds) || inRange(newBase + newSize + 2, this.segmentList[i].base, this.segmentList[i].bounds))) {
+      //   return false;
+      // } else if (segment.growDirection === 1 && ((inRange(newBase, this.segmentList[i].base, this.segmentList[i].bounds) && this.segmentList[i].growDirection === 1) || (inRange(newBase + 1, this.segmentList[i].base, this.segmentList[i].bounds) && this.segmentList[i].growDirection === -1) || (inRange(newBase + newSize, this.segmentList[i].base, this.segmentList[i].bounds)))) {
+      //   return false;
+      // }
+
+      if (segmentGrowDirection === growDirection.Negative && (inRange(newBase, this.segmentList[i].base, this.segmentList[i].bounds))) {
+        return false;
+      } else if (segmentGrowDirection === growDirection.Positive && (inRange(newBase, this.segmentList[i].base, this.segmentList[i].bounds))) {
+        return false;
+      }
+
+      if (segmentGrowDirection === growDirection.Negative && (inRange(newBase + newSize * segmentGrowDirection, this.segmentList[i].base, this.segmentList[i].bounds))) {
+        return false;
+      } else if(segmentGrowDirection == growDirection.Positive && (inRange(newBase + newSize, this.segmentList[i].base, this.segmentList[i].bounds))) {
+        return false;
+      }
+    }
+  
+    return true;
+  }
+
+  addNewVAS(vas: InstanceType<typeof VirtualAddressSpace>) {
+    // if (this.addressSpaceList.length < this.maxAddressSpaces) {
+    if(this.getPercentFull() < 1) {
+      vas.vaLength = this.vaLength;
+      vas.size = this.vaSize;
+      vas.id = this.vasId++;
+      this.addressSpaceList.push(vas);
+    } else {
+      throw new Error("Too many virtual address spaces");
+    }
+  }
+
+  editVaLength(newVaLength: number) {
+    let vaSize = 2 ** newVaLength;
+
+    for (let i = 0; i < this.addressSpaceList.length; i++) {
+      this.addressSpaceList[i].vaLength = newVaLength;
+      this.addressSpaceList[i].size = vaSize;
+      // call vas function to update all vbase and vbounds of segments
+      for (let j = 0; j < this.addressSpaceList[i].segmentList.length; j++) {
+        this.addressSpaceList[i].placeSegment(this.addressSpaceList[i].segmentList[j])
+      }
+    }
+
+    this.vaLength = newVaLength;
+    this.vaSize = 2 ** newVaLength;
+
+    this.maxAddressSpaces = (this.paLength - this.vaLength) === 0 ? 1 : (this.paLength - this.vaLength) * 2;
+  }
+
+  editPaLength(newPaLength: number) {  
+    this.paLength = newPaLength;
+    this.paSize = 2 ** newPaLength;
+
+    this.maxAddressSpaces = (this.paLength - this.vaLength) === 0 ? 1 : (this.paLength - this.vaLength) * 2; 
+  }
+
+  getPercentFull() {
+    let segmentSizeSum = 0;
+
+    for (let i = 0; i < this.segmentList.length; i++) {
+      segmentSizeSum += this.segmentList[i].size;
+    }
+
+    return segmentSizeSum / this.paSize;
+  }
+}
+
+class VirtualAddressSpace {
+  size: number;
+  vaLength: number;
+  id: number;
+  segmentList: Array<Segment>
+
+  constructor() {
+    // All three set when VAS is added to PAS
+    this.vaLength = 0;
+    this.size = 0;
+    // Used to keep track of segments in visualization
+    this.id = 0;
+
+    this.segmentList = [];
+  }
+ 
+  // When creating a new segment, the new segment's VABase won't be known because that would be given to it
+  validSegmentCreationOrChange(segment: InstanceType<typeof Segment>, newSize: number) {
+    if (newSize > this.size / 4) {
+      return false;
+    }
+
+    // Seeing if virtual address space already has this type of segment and whether it is the current segment or not
+    if(!(this.segmentList.indexOf(segment) === -1) && this.segmentList[this.segmentList.indexOf(segment)] !== segment) {
+      return false;
+    }
+
+
+    // Should not be passing in newVABase because that is assigned by the VirtualAddressSpace
+    if (newSize > (this.size / 4)) {
+      return false;
+    }
+
+    return true;
+  }
+
+  // Place segment works with growDirection incorrectly
+  placeSegment(segment: InstanceType<typeof Segment>) {
+    if (segment.base < 0) {
+      segment.vaBase = segment.vaBounds = -1;
+      return;
+    }
+
+    if (segment.type === segmentType.code) {
+      if (segment.growDirection === growDirection.Negative) {
+        segment.vaBase = this.size * (1/4);
+      } else {
+        segment.vaBase = 0;
+      }
+    }
+
+    else if (segment.type === segmentType.heap) {
+      if (segment.growDirection === growDirection.Negative) {
+        segment.vaBase = this.size * (1/2);
+      } else {
+        segment.vaBase = this.size * (1/4);
+      }
+    }
+
+    else if (segment.type === segmentType.stack) {
+      if (segment.growDirection === growDirection.Negative) {
+        segment.vaBase = this.size * (3/4);
+      } else {
+        segment.vaBase = this.size * (1/2);
+      }
+    }
+    
+    else if (segment.type === segmentType.extra) {
+      if (segment.growDirection === growDirection.Negative) {
+        segment.vaBase = this.size - 1;
+      } else {
+        segment.vaBase = this.size * (3/4);
+      }
+    }
+    // Theoretically can never reach this section
+    else {
+      throw new Error("Segment must have a type")
+    }
+
+    if (segment.growDirection === growDirection.Positive) {
+      segment.vaBounds = segment.vaBase + segment.size - 1;
+    } else {
+      segment.vaBounds = segment.vaBase - segment.size;
+    }
+
+  }
+
+  addNewSegment(segment: InstanceType<typeof Segment>) {
+    this.placeSegment(segment);
+    this.segmentList.push(segment);
+  }
+
+  editSegment(segment: InstanceType<typeof Segment>, segmentIndex: number) {
+    // To reuse addNewSegment function create copy of segment and delete in array
+    // let editedSegment = segment;
+    //
+    // this.segmentList.splice(this.segmentList.indexOf(segment), 0);
+    //
+    // this.addNewSegment(editedSegment);
+
+    this.placeSegment(segment);
+    this.segmentList[segmentIndex] = segment;
+
+  }
+
+  editSegmentGrowDirection(segment: InstanceType<typeof Segment>, segmentIndex: number, growDirection: number) {
+    this.placeSegment(segment);
+    this.segmentList[segmentIndex] = segment;
+  }
+
+  addressInSegment(virtualAddress: number) {
+    for (let i = 0; i < this.segmentList.length; i++) {
+      // With negative growth direction, the first valid address is segmentBase - 1, 
+      if (validAccess(this.segmentList[i].vaBase, this.segmentList[i].vaBounds, virtualAddress, this.segmentList[i].growDirection)) {
+        return this.segmentList[i];
+      }
+    }
+
+    // If I want to be typesafe about this, change this to return null and handle that in sim or page 
+    return "Segmentation Fault";
+  }
+
+  // Could be named better
+  translateVirtualToPhysicalAddress(virtualAddress: number): string {
+    const segmentFound = this.addressInSegment(virtualAddress);
+
+    if (segmentFound === "Segmentation Fault") {
+      return "Segmentation Fault"
+    } else {
+      return segmentFound.type.name
+    }
+  }
+}
+
+class Segment {
+  size: number;
+  base: number;
+  bounds: number;
+  vaBase: number;
+  vaBounds: number;
+  growDirection: typeof growDirection;
+  type: typeof segmentType;
+
+  constructor(type: typeof segmentType, base: number, size: number, segmentGrowDirection: number)  {
+    this.type = type;
+    this.size = size;
+
+    // Physical Address Space Information
+    this.base = base;
+    
+    // Should be one less than base + size because we want n values instead of n+1
+    this.bounds = boundsCalc(this.base, this.size, segmentGrowDirection);
+
+    
+    // Virtual Address Space Information
+    this.vaBase = 0;
+    this.vaBounds = 0;
+
+    this.growDirection = segmentGrowDirection;
+  }
+}
+
+export default {PhysicalAddressSpace, VirtualAddressSpace, Segment}
