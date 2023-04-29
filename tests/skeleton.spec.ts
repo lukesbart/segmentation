65207d2b45d2d2386094817cd706b6250e16d762
Luke Bartholomew <lukesbart@icloud.com>
2023-04-29 14:45:51 -0400

    Add tests for all negative and positive growth segments

--- tests/skeleton.spec.ts
+++ tests/skeleton.spec.ts
@@ -1,8 +1,9 @@
 import {describe, expect, it} from 'vitest';
-import pkg from '../src/routes/skeleton.js'
-const { PhysicalAddressSpace, VirtualAddressSpace, Segment} = pkg; 
-import segmentType from '../src/routes/constants/segmentType.js';
-import growDirection from '../src/routes/constants/growDirection.js';
+
+import { PhysicalAddressSpace, VirtualAddressSpace, Segment } from '../src/routes/skeleton';
+
+import segmentType from '../src/routes/constants/segmentType';
+import growDirection from '../src/routes/constants/growDirection';
 
 const addSegmentToPAS = (pas: InstanceType<typeof PhysicalAddressSpace>, newSegment: InstanceType<typeof Segment>): void => {
     if (pas.validSegmentCreationOrChange(newSegment, newSegment.base, newSegment.size, newSegment.growDirection)) {
@@ -77,6 +78,85 @@ describe("PAS validSegmentCreationOrChange", () => {
     })
 })
 
+describe("PAS validSegmentCreationOrChange with all positive growth segments", () => {
+    const pas = new PhysicalAddressSpace(12, 10);
+
+    const pasCode = new Segment(segmentType.code, 0, 256, growDirection.Positive);
+    const pasHeap = new Segment(segmentType.heap, 256, 256, growDirection.Positive);
+    const pasStack = new Segment(segmentType.stack, 512, 256, growDirection.Positive);
+    const pasExtra = new Segment(segmentType.extra, 768, 256, growDirection.Positive);
+    
+    it("Added the code segment to the address space", () => {
+        addSegmentToPAS(pas, pasCode);
+        expect(pas.segmentList.length).toBe(1)
+        expect(pas.segmentList).toContain(pasCode)
+    })
+
+    it("Added the heap segment to the address space", () => {
+        addSegmentToPAS(pas, pasHeap);
+        expect(pas.segmentList.length).toBe(2)
+        expect(pas.segmentList).toContain(pasHeap)
+    })
+
+    it("Added the stack segment to the address space", () => {
+        addSegmentToPAS(pas, pasStack);
+        expect(pas.segmentList.length).toBe(3)
+        expect(pas.segmentList).toContain(pasStack)
+    })
+
+    it("Added the extra segment to the address space", () => {
+        addSegmentToPAS(pas, pasExtra);
+        expect(pas.segmentList.length).toBe(4)
+        expect(pas.segmentList).toContain(pasExtra)
+    })
+})
+
+
+describe("PAS validSegmentCreationOrChange with all negative growth segments", () => {
+    const pas = new PhysicalAddressSpace(12, 10);
+
+    // Can two negative growth segments share a base and a bounds?
+
+    const pasCode = new Segment(segmentType.code, 256, 256, growDirection.Negative);
+    const pasHeap = new Segment(segmentType.heap, 512, 256, growDirection.Negative);
+    const pasStack = new Segment(segmentType.stack, 768, 256, growDirection.Negative);
+    // Can a negative growth segment have a bounds at 1024?
+    const pasExtra = new Segment(segmentType.extra, 1024, 256, growDirection.Negative);
+
+    it("Added the code segment to the address space",  () => {
+        addSegmentToPAS(pas, pasCode)
+        expect(pas.segmentList.length).toBe(1);
+        expect(pas.segmentList).toContain(pasCode);
+    })
+
+    it("Added the heap segment to the address space", () => {
+        addSegmentToPAS(pas, pasHeap);
+        expect(pas.segmentList.length).toBe(2);
+        expect(pas.segmentList).toContain(pasHeap);
+    })
+
+
+    it("Added the stack segment to the address space", () => {
+        addSegmentToPAS(pas, pasStack);
+        expect(pas.segmentList.length).toBe(3);
+        expect(pas.segmentList).toContain(pasStack);
+    })
+
+   it("Added the extra segment to the address space", () => {
+        addSegmentToPAS(pas, pasExtra);
+        expect(pas.segmentList.length).toBe(4);
+        expect(pas.segmentList).toContain(pasExtra);
+    })
+
+    it("Tries to overlap code and heap", () => {
+        expect(pas.validSegmentCreationOrChange(pasHeap, 500, 256, growDirection.Negative)).toBeFalsy()
+    })
+
+    it("Tries to move code out from current location and back", () => {
+        expect(pas.validSegmentCreationOrChange(pasCode, 2048, 256, growDirection.Negative)).toBeTruthy()
+        expect(pas.validSegmentCreationOrChange(pasCode, 256, 256, growDirection.Negative)).toBeTruthy()
+    })
+})
 
 // describe("PAS", () => {
 //     const pas = new PhysicalAddressSpace(12, 10);
