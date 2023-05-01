1e245d42b764d5760a2780778106006e719bd3cf
Luke Bartholomew <lukesbart@icloud.com>
2023-05-01 17:28:42 -0400

    Update prerender, adapter settings

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,6 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
 
 <script>
-    export const prerender = true;
     import "../app.css"
 </script>
 
