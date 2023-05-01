fbd7137fcc671070127c1c79e23d3e8006ca14ee
Luke Bartholomew <lukesbart@icloud.com>
2023-05-01 18:54:53 -0400

    Add pages about architecture and simulation

---
+++ src/routes/about_architecture/+page.svelte
@@ -0,0 +1,29 @@
+<title>About Simulator Architecture</title>
+
+<h1 class="text-center text-5xl mb-2">About Segmentation</h1>
+<div class="text-center font-bold mb-10">
+    <a href="/" class="text-blue-500">Simulator</a>
+    <a href="/about_segmentation" class="text-red-500">About Segmenation</a>
+</div>
+
+<!-- <embed src="/static/Simulator Architecture.pdf" type="application/pdf"> -->
+<div class="flex justify-center mb-2">
+    <img src="/simarch.png" alt="" style="height: 450px; width: 3/4">
+</div>
+    
+<main>
+    <h2 class="text-4xl text-center">Segment</h2>
+    <p class="text-justify container mx-auto mb-4">Contains information about itself, information that is necessary for it to function in a Physical Address Space and a Virtual Address Space. Has one VAS and one PAS.</p>
+
+    <h2 class="text-4xl text-center">Virtual Address Space</h2>
+    <p class="text-justify container mx-auto mb-4">Contains information necessary for it to work inside of a Physical Address Space. Has methods to manage the Segments inside of it. Has one PAS and many Segments.</p>
+   
+    <h2 class="text-4xl text-center">Physical Address Space</h2>
+    <p class="text-justify container mx-auto mb-4">Contains information necessary for it to work inside a simulator. Has methods to manage segments and address spaces within it. Has many Virtual Address Spaces and Segments. Has one Simulator</p>
+
+    <h2 class="text-4xl text-center">Simulator</h2>
+    <p class="text-justify container mx-auto mb-4">Contains method to manage Physical Address Space, Virtual Address Spaces, and Segments. Before making any changes on segments, Virtual Address Space and Physical Address Space validation functions are called. Also provides logic to manage Virtual Address Spaces and the Physical Address Space and validate changes to them. Provides methods to turn a Physical Address Space into JSON and turn JSON back into a complete Physical Address Space. Simulator contains one Physical Address Space, which is provided to the visualization client.</p>
+
+    <h2 class="text-4xl text-center">Philosophy</h2>
+    <p class="text-justify container mx-auto mb-4">Logic for simulator and presentation are kept as separate as possible. The goal of this is to allow for the simulator code to be used and expanded on independently of any view code. This was chosen to try to make the Simulator more maintainable. All that must be done to switch out the view is to render the Physical Address Space and provide ways for a user to change the Physical Address Space and things it contains with the functions given in the simulator and reflect those changes in the rendering of the Physical Address Space</p>
+</main>
---
+++ src/routes/about_segmentation/+page.svelte
@@ -0,0 +1,12 @@
+<title>About Segmentation</title>
+
+<h1 class="text-center text-5xl mb-4">About Segmentation</h1>
+<div class="text-center font-bold mb-10">
+    <a href="/" class="text-blue-500">Simulator</a>
+    <a href="/about_architecture" class="text-red-500">Architecture of Simulator</a>
+</div>
+
+<main>
+    <h2 class="text-4xl text-center">Virtual Address Space</h2>
+    <p class="text-justify container mx-auto">The virtual address space is an abstration provided to the program</p>
+</main>
